07514b75cc829637ca8a0b2328426c5f848ed971
Felix Frei <felix.frei@gmx.ch>
2024-06-16 08:36:27 +0200

    generate_invoice.py added

---
+++ generate_invoice.py
@@ -0,0 +1,16 @@
+"""
+Generate invoice for test payment
+"""
+from pyln.client import LightningRpc
+import random
+
+# Create an instances of the LightningRpc object
+node = LightningRpc("/root/.lightning/lnd-rpc-file")
+
+info_A = node.getinfo()
+print(info_A)
+
+# Create invoice for test payment
+invoice = node.invoice(18000, "lbl{}".format(random.random()), "testpayment")
+
+print(invoice)
