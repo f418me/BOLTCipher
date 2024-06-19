e64fa60cb0558f7cab2d6efec932ec0a53f896ee
Felix Frei <felix.frei@gmx.ch>
2024-06-19 06:29:09 +0200

    HTML responses added

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # BOLTChipher
 
-uvicorn paydec:app --reload
+uvicorn main:app --reload
+
+ sudo docker compose up -d 
+
+docker compose build --no-cache
+
+docker compose 
+
+fre@30107:~$ sudo docker exec ln-docker-lightning-1 lightning-cli connect 
+
+
+sudo docker exec ln-docker-lightning-1 lightning-cli fundchannel 02f1a8c87607f415c8f22c00593002775941dea48869ce23096af27b0cfdcc0b69 1000000 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import binascii
-import random
 from base64 import b64encode
+from random import random
 
+from pyln.client import LightningRpc
 from fastapi import FastAPI
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
@@ -9,9 +10,15 @@ from Crypto.Random import get_random_bytes
 from pydantic import BaseModel
 import uuid
 
-app = FastAPI()
-from pyln.client import LightningRpc
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 def encrypt_message_b64(data, key, iv):
     cipher = AES.new(key, AES.MODE_CFB, iv)
@@ -35,41 +42,117 @@ class abstractItem(BaseModel):
     abstract: str
 
 
-@app.get("/abstract", response_model=abstractItem)
-async def get_abstract():
-    with open('abstract.txt', 'r', encoding='utf-8') as file:
-        abstract = file.read()
-    return abstractItem(encrypt_mode="AES.MODE_CFB", cost="Invoice123",
-                       abstract=abstract)
-
-
-# Route, die das JSON-Objekt zurückgibt
-@app.get("/content/", response_model=contentItem)
-async def get_whitepaper():
-    with open('content.txt', 'r', encoding='utf-8') as file:
+@app.get("/content", response_class=HTMLResponse)
+async def get_content(request: Request):
+    with open('content_planB.txt', 'r', encoding='utf-8') as file:
         # Lesen aller Zeilen der Datei
         content = file.read()
         encoded_content = content.encode('utf-8')
 
-    with open('abstract.txt', 'r', encoding='utf-8') as file:
+    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
+        abstract = file.read()
+
+    node = LightningRpc("/root/.lightning/lnd-rpc-file")
+
+    invoice = node.invoice(18000, "lbl{}".format(random.random()), "BOLTCipher")
+    bolt11 = invoice['bolt11']
+
+    unique_id = str(uuid.uuid4())
+    hex_key = invoice['payment_secret']
+    key = binascii.unhexlify(hex_key)
+    iv = get_random_bytes(16)
+    iv_hex = iv.hex()
+    encrypt_content = encrypt_message_b64(encoded_content, key, iv)
+    return templates.TemplateResponse(
+        request=request, name="response.html", context={"unique_id":unique_id, "encrypt_mode": encrypt_mode, "cost": cost, "abstract": abstract, "bolt11": bolt11, "iv_hex": iv_hex, "encrypted_content": encrypted_content}
+    )
+
+
+
+
+@app.get("/content-json", response_model=contentItem)
+async def get_content_json():
+    with open('content_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        content = file.read()
+        encoded_content = content.encode('utf-8')
+
+    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
         # Lesen aller Zeilen der Datei
         abstract = file.read()
 
     node = LightningRpc("/root/.lightning/lnd-rpc-file")
 
-    invoice = node.invoice(18000, "lbl{}".format(random.random()), "testpayment")
-    print(invoice)
-    print("")
-    print(f"bolt11: {invoice['bolt11']}")
-    print(f"payment_secret: {invoice['payment_secret']}")
+    invoice = node.invoice(18000, "lbl{}".format(random.random()), "BOLTCipher")
     bolt11 = invoice['bolt11']
 
-    # Generierung einer UUID im RFC 4122 Format
     unique_id = str(uuid.uuid4())
     hex_key = invoice['payment_secret']
     key = binascii.unhexlify(hex_key)
-    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
+    iv = get_random_bytes(16)
     iv_hex = iv.hex()
     encrypt_content = encrypt_message_b64(encoded_content, key, iv)
     return contentItem(id=unique_id, encrypt_mode="AES.MODE_CFB", initialize_vector_hex=iv_hex, invoice=bolt11,
                        abstract=abstract, content=encrypt_content)
+
+
+@app.get("/test-content", response_class=HTMLResponse)
+async def get_test_content(request: Request):
+    with open('content_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        content = file.read()
+        encoded_content = content.encode('utf-8')
+
+    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        abstract = file.read()
+
+    # Generierung einer UUID im RFC 4122 Format
+    unique_id = str(uuid.uuid4())
+    encrypt_mode = "AES.MODE_CFB"
+    cost = "18 Sats"
+    hex_key = '016ede0e62833ff4088a60f8b7823943ba641b0da11555997f434734e860738d'
+    bolt11 = "lnbc1800n1"
+    key = binascii.unhexlify(hex_key)
+    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
+    iv_hex = iv.hex()
+    encrypted_content = encrypt_message_b64(encoded_content, key, iv)
+    return templates.TemplateResponse(
+        request=request, name="response.html", context={"unique_id":unique_id, "encrypt_mode": encrypt_mode, "cost": cost, "abstract": abstract, "bolt11": bolt11, "iv_hex": iv_hex, "encrypted_content": encrypted_content}
+    )
+
+@app.get("/test-content-json/", response_model=contentItem)
+async def get_content_json():
+    with open('content_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        content = file.read()
+        encoded_content = content.encode('utf-8')
+
+    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        abstract = file.read()
+
+    # Generierung einer UUID im RFC 4122 Format
+    unique_id = str(uuid.uuid4())
+    hex_key = '016ede0e62833ff4088a60f8b7823943ba641b0da11555997f434734e860738d'
+    key = binascii.unhexlify(hex_key)
+    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
+    iv_hex = iv.hex()
+    encrypt_content = encrypt_message_b64(encoded_content, key, iv)
+    return contentItem(id=unique_id, encrypt_mode="AES.MODE_CFB", initialize_vector_hex=iv_hex, invoice="Invoice123",
+                       abstract=abstract, content=encrypt_content)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_info(request: Request):
+    encrypt_mode = "AES.MODE_CFB"
+    cost = "18 Sats"
+
+    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
+        # Lesen aller Zeilen der Datei
+        abstract = file.read()
+
+    return templates.TemplateResponse(
+        request=request, name="info.html", context={"encrypt_mode": encrypt_mode, "cost": cost, "abstract": abstract}
+    )
+
