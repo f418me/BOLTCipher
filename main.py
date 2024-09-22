9b6d754c7a60001ab5014b6d66e07ce394cbe761
Felix Frei <felix.frei@gmx.ch>
2024-09-22 18:40:58 +0200

    File download added

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import binascii
-import csv
+import io
 import logging
 import secrets
+import subprocess
 from base64 import b64encode
-from pyln.client import LightningRpc
 from fastapi import FastAPI
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-
 from pydantic import BaseModel, Field
 import uuid
-
 from starlette.requests import Request
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, FileResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-
 from config import Config
 from utils.cln import get_invoice
-
-#todo put all strings into config
+from fastapi.responses import StreamingResponse
+import zipfile
 
 
 config = Config()
@@ -29,32 +25,6 @@ log = logging.getLogger(__name__)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-#todo remove or move to different class
-def read_bolt11(file_path):
-    updated_rows = []
-    result_dict = None
-    found_unread = False  # Zustand, um zu überprüfen, ob eine ungelesene Zeile gefunden wurde
-
-    # CSV-Datei öffnen und Zeilen lesen
-    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
-        reader = list(csv.reader(file))
-        for row in reader:
-            if not found_unread:  # Prüfen, ob bereits eine ungelesene Zeile gefunden wurde
-                if len(row) == 2 and not row[0].startswith(
-                        'used'):  # Überprüfen, ob die Zeile zwei Elemente hat und nicht markiert ist
-                    result_dict = {'payment_secret': row[0], 'bolt11': row[1]}
-                    row = ['used'] + row  # Markieren der Zeile als "used" am Anfang der Zeile
-                    found_unread = True  # Setzen des Flags, dass eine ungelesene Zeile gefunden wurde
-            updated_rows.append(row)  # Füge die gelesenen Zeilen zur Aktualisierungsliste hinzu
-
-    # CSV-Datei mit den aktualisierten Zeilen zurückschreiben
-    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerows(updated_rows)
-
-    return result_dict
-
 
 def encrypt_message_b64(data, key, iv):
     cipher = AES.new(key, AES.MODE_CFB, iv)
@@ -67,20 +37,12 @@ class ContentItem(BaseModel):
     unique_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     encrypt_mode: str = 'AES.MODE_CFB'
     cost: str = f'{config.CONTENT_PRICE} Sats'
-    initialize_vector_hex: str
     bolt11: str
     abstract: str
     encrypted_content: str
 
 
-class AbstractItem(BaseModel):
-    encrypt_mode: str
-    cost: str
-    abstract: str
-
-
-@app.get("/content", response_class=HTMLResponse)
-async def get_content(request: Request):
+def get_content():
     with open('content.txt', 'r', encoding='utf-8') as file:
         content = file.read()
         encoded_content = content.encode('utf-8')
@@ -104,8 +66,6 @@ async def get_content(request: Request):
     log.info(f"preimage: {preimage}")
     log.info(f"bolt11: {bolt11}")
 
-
-
     encrypted_content = encrypt_message_b64(encoded_content, key, iv)
 
     content_item = ContentItem(
@@ -114,118 +74,32 @@ async def get_content(request: Request):
         initialize_vector_hex=iv_hex,
         encrypted_content=encrypted_content
     )
-
-    return templates.TemplateResponse(
-        request=request, name="response.html",
-        context={"request": request, "item": content_item.dict()}
-    )
+    return content_item
 
 
-#todo update json response according to the content route
-@app.get("/content-json", response_model=ContentItem)
+@app.get("/json/", response_model=ContentItem)
 async def get_content_json():
-    with open('content.txt', 'r', encoding='utf-8') as file:
-        # Lesen aller Zeilen der Datei
-        content = file.read()
-        encoded_content = content.encode()
-
-    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
-        # Lesen aller Zeilen der Datei
-        abstract = file.read()
-
-    node = LightningRpc("/root/.lightning/lnd-rpc-file")
-
-    preimage = secrets.token_hex(32)
-
-    invoice = node.invoice(
-        amount_msat=18000,
-        #label="lbl{}".format(random.random()),
-        description="BOLTCipher",
-        preimage=preimage
-    )
-    bolt11 = invoice['bolt11']
-
-    #invoice = read_bolt11('bolt11.csv')
-    #bolt11 = invoice['bolt11']
-    key = binascii.unhexlify(preimage)
-    iv = get_random_bytes(16)
-    iv_hex = iv.hex()
-    encrypted_content = encrypt_message_b64(encoded_content, key, iv)
-
-    preimage = '016ede0e62833ff4088a60f8b7823943ba641b0da11555997f434734e860738d'
-    key = binascii.unhexlify(preimage)
-    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
-    iv_hex = iv.hex()
-    encrypted_content = encrypt_message_b64(encoded_content, key, iv)
-
-    return ContentItem(
-        initialize_vector_hex=iv_hex,
-        abstract=abstract,
-        bolt11=bolt11,
-        encrypted_content=encrypted_content
-    )
-
-
-@app.get("/test-content", response_class=HTMLResponse)
-async def get_test_content(request: Request):
-    with open('content.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-        encoded_content = content.encode('utf-8')
-
-    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
-        abstract = file.read()
-
-    preimage = '016ede0e62833ff4088a60f8b7823943ba641b0da11555997f434734e860738d'
-    bolt11 = "lnbc1800n1"
-    key = binascii.unhexlify(preimage)
-    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
-    iv_hex = iv.hex()
-    encrypted_content = encrypt_message_b64(encoded_content, key, iv)
-    content_item = ContentItem(
-        abstract=abstract,
-        bolt11=bolt11,
-        initialize_vector_hex=iv_hex,
-        encrypted_content=encrypted_content
-    )
-    return templates.TemplateResponse(
-        request=request, name="response.html",
-        context={"request": request, "item": content_item.dict()}
-    )
-
-
-@app.get("/test-content-json/", response_model=ContentItem)
-async def get_content_json():
-    with open('content.txt', 'r', encoding='utf-8') as file:
-        # Lesen aller Zeilen der Datei
-        content = file.read()
-        encoded_content = content.encode('utf-8')
-
-    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
-        # Lesen aller Zeilen der Datei
-        abstract = file.read()
-
-    # Generierung einer UUID im RFC 4122 Format
-    unique_id = str(uuid.uuid4())
-    hex_key = '016ede0e62833ff4088a60f8b7823943ba641b0da11555997f434734e860738d'
-    cost = "18 Sats"
-    ecrypt_mode = "AES.MODE_CFB"
-    key = binascii.unhexlify(hex_key)
-    iv = get_random_bytes(16)  # Zufälliger Initialisierungsvektor
-    iv_hex = iv.hex()
-    encrypt_content = encrypt_message_b64(encoded_content, key, iv)
-    return ContentItem(id=unique_id, encrypt_mode="AES.MODE_CFB", initialize_vector_hex=iv_hex, invoice="Invoice123",
-                       abstract=abstract, content=encrypt_content)
+    content_item = get_content()
+    return content_item
 
 
 @app.get("/", response_class=HTMLResponse)
 async def get_info(request: Request):
-    encrypt_mode = "AES.MODE_CFB"
-    cost = "18 Sats"
-
-    with open('abstract_planB.txt', 'r', encoding='utf-8') as file:
-        # Lesen aller Zeilen der Datei
-        abstract = file.read()
+    content_item = get_content()
 
     return templates.TemplateResponse(
-        request=request, name="info.html", context={"encrypt_mode": encrypt_mode, "cost": cost, "abstract": abstract}
+        request=request, name="response.html",
+        context={"request": request, "item": content_item.dict()}
     )
+
+
+@app.get("/download-pdf-in-zip")
+def download_pdf_in_zip():
+
+    pdf_file_path = "bitcoin.pdf"
+    zip_file_path = pdf_file_path + '.zip'
+    pwd = 'mypassword'
+    path = '.'
+    subprocess.run(["zip", "-P", pwd, '-r', zip_file_path, path])
+
+    return FileResponse(zip_file_path, media_type="application/zip", filename=zip_file_path)
